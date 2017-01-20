8203d72488f6588db05e916c60eefa6324169024
Tim Baldwin <baldwintm@gmail.com>
2017-01-20 13:29:42 -0500

    updated nest script to use rest API to set index retention, clean up unnecessary imports, comments and function calls

--- bin/devices.py
+++ bin/devices.py
@@ -1,13 +1,9 @@
 from multiprocessing import Process
 import requests
-import requests.auth
 import os
 import time
-import datetime
 import splunk.clilib.cli_common
 import json
-import ast
-import signal
 import sys
 import platform
 import subprocess
@@ -21,83 +17,66 @@ class Unbuffered:
         self.stream.flush()
     def __getattr__(self, attr):
         return getattr(self.stream, attr)
-def check_splunk(process_id,self_pid,procs):
-	#Assume Splunk is running
-        splunk_running = True
-        #Assume devices.py is running
-        devices_running = True
-        
-        #Output message regarding this check
-        sys.stderr.write("entering check_splunk\n")
-        
-        #While splunk is supposedly running and devices is running, do some stuff
-        while splunk_running and devices_running:
-        
-        	#Try to kill the splunk pid to see if it's still alive
-		try:
-			os.kill(int(process_id), 0)
-		#If it's not, notify us and change splunk to not running
-		except OSError:
-			sys.stderr.write("error: detected splunk not running\n")
-			splunk_running = False
-			continue
-		#If Splunk isn't running, do some more checks
-		else:
-			#Assume Splunk is running again
-			splunk_running = True
-				#For all of the processes in the subprocess list
-                for p in procs:
-                		#If each process isn't running, detect it's not running and set variable
-                        if not p.is_alive():
-                            sys.stderr.write("error: detected devices spawn no longer running\n")
-                            devices_running = False
-                #If the process is running, go back to sleep
-                time.sleep(1)
-        #If Splunk isn't running
-        if splunk_running is False:
-            try:
-            	#try to kill some processes
-                for p in procs:
-                    p.terminate()
-            #If can't kill it, assume it's terminated
-            except IOError: # proc has already terminated
-                pass
-        return True
+def logger(message):
+    sys.stderr.write(message.strip() + "\n")
+
+def check_splunk(process_id,procs):
+    #initialize variables
+    splunk_running = True
+    devices_running = True
+    
+    # keep checking that splunkd and child procs are still alive
+    while splunk_running and devices_running:
+        #check that the splunk process is alive
+        try:
+            os.kill(int(process_id), 0)
+        #If it's not alive, notify and drop out of the loop
+        except OSError:
+            logger("ERROR detected splunk not running")
+            splunk_running = False
+            continue
+        for p in procs:
+            #If any of the child processes isn't running, notify and drop out of the loop
+            if not p.is_alive():
+                logger("ERROR detected child process for devices longer running")
+                devices_running = False
+        #If the processes are all running, go back to sleep
+        time.sleep(1)
+    return True
+
 def get_devices(access_token):
     headers = {"Authorization": "bearer ", "Accept": "text/event-stream"}
     response = requests.get("https://developer-api.nest.com/?auth=" + access_token, headers=headers, stream=True, timeout=3600)
     for line in response.iter_lines():
-		#ts = time.time()
-		#st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-		if line == 'event: put':
-			continue
-		if line == 'event: keep-alive':
-			continue
-		if line == 'data: null':
-			continue
-		output_str = line.replace('data: {"path"','{"path"')
-		#print(str(output_str))
-		sys.stdout.write(output_str)
+        if line == 'event: put':
+            continue
+        if line == 'event: keep-alive':
+            continue
+        if line == 'data: null':
+            continue
+        output_str = line.replace('data: {"path"','{"path"')
+        sys.stdout.write(output_str)
     return True
 
 def enforce_retention(sessionKey):
     #ensure the Nest Index Retention is only 10 days
     if len(sessionKey) == 0:
-       sys.stderr.write("Did not receive a session key. Please enable passAuth in inputs.conf for this script\n")
+       logger("ERROR Did not receive a session key. Please enable passAuth in inputs.conf for this script")
        exit(2)
-
+    
     try:
         nest_input = splunk.rest.simpleRequest('/services/data/inputs/script/.%252Fbin%252Fdevices.py?output_mode=json', method='GET', sessionKey=sessionKey, raiseAllErrors=True)
     except Exception:
-        sys.stderr.write("input doesn't exist\n")
+        logger("INFO Nest devices.py input doesn't exist")
 
     nest_input_json = json.loads(nest_input[1])
     nest_index_name = nest_input_json['entry'][0]['content']['index']
-
+    nest_sourcetype = nest_input_json['entry'][0]['content']['sourcetype']
+    
     try:
         nest_index = splunk.rest.simpleRequest('/services/data/indexes/' + nest_index_name  + '?output_mode=json', method='GET', sessionKey=sessionKey, raiseAllErrors=True)
     except Exception:
-        sys.stderr.write("index doesn't exist\n")
+        logger("INFO " + nest_index_name + " index doesn't exist")
     
     nest_json = json.loads(nest_index[1])
     nest_frozen_time = nest_json['entry'][0]['content']['frozenTimePeriodInSecs']
@@ -105,82 +84,36 @@ def enforce_retention(sessionKey):
     
     postArgs = {"frozenTimePeriodInSecs": 864000}
     if nest_frozen_time > 864000:
-        sys.stderr.write("nest index retention is too high, adjusting down to 10 days\n")
+        logger("INFO nest index retention is too high, adjusting down to 10 days")
         splunk.rest.simpleRequest(index_edit_list, method='POST', sessionKey=sessionKey, raiseAllErrors=True, postargs=postArgs)
-
+    
     return True
 
-
-#Set stdout to Unbuffered Version
+#set initial veriables
 sys.stdout = Unbuffered(sys.stdout)
-
-#Get Splunk Home
 splunk_home = os.path.expandvars("$SPLUNK_HOME")
-#Create Variable to Track Splunk Pid
-splunk_pid = ""
-
-#Create Variable to Track Current Python Script Pid
-current_pid = ""
-current_pid = os.getpid()
-
-#Get OS
-OS = platform.platform()
-
-#get splunk session key
+splunk_pid = open(os.path.join(splunk_home,"var","run", "splunk", "conf-mutator.pid"), 'rb').read()
 sessionKey = sys.stdin.readline().strip()
+logger("variables initialized")
+
+#enforce the required retention policy
 enforce_retention(sessionKey)
 
 #start the real work
-#What to do for Macs
-if "Darwin" in OS:
-	processes = subprocess.Popen(['ps','aux'], stdout=subprocess.PIPE).stdout.readlines() 
-	for process in processes:
-		try:
-			if 'splunkd' in process:
-				proc_info = process.split(' ')
-				pid = proc_info[6]
-				splunk_pid = pid
-				break
-			else:
-					splunk_pid = "SPLUNK NOT RUNNING"
-		except IOError: # proc has already terminated
-		   continue
-
-	if "SPLUNK NOT" in splunk_pid:
-			sys.exit()
-#What to do for anything else
-else:
-	#Get All Current Pids
-	pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
-
-	#Check if Splunk is Running and Die if not
-	for pid in pids:
-		cmd = open(os.path.join('/proc', pid, 'cmdline'), 'rb').read()
-		try:
-			if 'splunkd' in cmd:
-					splunk_pid = pid
-					break
-			else:
-					splunk_pid = "SPLUNK NOT RUNNING"
-		except IOError: # proc has already terminated
-		   continue
-
-	if "SPLUNK NOT" in splunk_pid:
-			sys.exit()
-
-#Read in all Access Tokens from Homeview.conf
+#Read in all Access Tokens from nest_tokens.conf
 proc = []
-settings = splunk.clilib.cli_common.readConfFile(splunk_home+"/etc/apps/NestAddonforSplunk/local/nest_tokens.conf")
+settings = splunk.clilib.cli_common.getMergedConf("nest_tokens")
 for item in settings.iteritems():
-        for key in item[1].iteritems():
-		token = key[1]
-		#Create a new process for each access_token
-		devices = Process(target=get_devices, args=(token,))
-		devices.start()
-		proc.append(devices)
+    for key in item[1].iteritems():
+        token = key[1]
+        #Create a new process for each nest key (access_token)
+        devices = Process(target=get_devices, args=(token,))
+        devices.start()
+        proc.append(devices)
+
 #Create a Process to Check if Splunk is running and kill all child processes if Splunk dies or Splunk PID Changes
-if check_splunk(splunk_pid,current_pid,proc):
-	for p in proc:
-		p.terminate()
-#If All Subprocesses Die, Kill Python Script
+if check_splunk(splunk_pid,proc):
+    for p in proc:
+        p.terminate()
+
 sys.exit()
